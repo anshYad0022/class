e21dd046a7258f1bc479913aad8c95667f6b3742
thejackie <thejackie2001@gmail.com>
2025-09-16 01:32:25 +0530

    update

---
+++ lec5/sumofdigits.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int lastdigit,num=946,sum=0;
+    while (num>0)
+    {
+    lastdigit=num%10;
+    sum=sum+lastdigit;
+    num=num/10;
+    }
+    printf("%d",sum);
+}
